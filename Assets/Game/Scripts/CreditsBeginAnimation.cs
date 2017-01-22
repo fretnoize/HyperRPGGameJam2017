5c7776dc8556a024916d79747c4d55688e9d6792
Nick <kedomir@gmail.com>
2017-01-22 10:57:51 +0000

    Added the opening credits.

---
+++ Assets/Game/Scripts/CreditsBeginAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsBeginAnimation : CreditsTools
+{
+    void Start()
+    {
+        StartCoroutine(DisplayCredits());
+    }
+
+    IEnumerator DisplayCredits()
+    {
+        string[] linesTyped = new string[] { "TeamThumper presents..." };
+        string[] linesFade = new string[] { "The Thumpening!", "(aka insert title here)" };
+
+        yield return BlankPause(1.0f);
+        yield return StartTyped(linesTyped, 3.0f);
+        yield return BlankPause(1.0f);
+        yield return StartFade(linesFade, 1.0f, 3.0f);
+        yield return BlankPause(1.0f);
+
+        //TODO scene transition into the main game here
+    }
+}
