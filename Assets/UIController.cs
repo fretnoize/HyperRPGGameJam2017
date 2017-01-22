89e785abc7527bc39a316ed9a97e059d0f0185f8
annamberg <annambe@stud.ntnu.no>
2017-01-22 04:25:34 +0100

    Basic Unity UI for main menu and options menu. Navigation between them, and ability to bring up the options during the game by using esc. Functionality to mute sound and adjust volume. Menus disappear when the game begins.

---
+++ Assets/UIController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIController : MonoBehaviour {
+
+	static GameObject[] menuObjects;
+	static GameObject[] optionsObjects;
+	static bool optionsMenu;
+	static bool mainMenu;
+
+	AudioSource audio;
+	public Slider volumeSlider;
+	public AudioSource volumeAudio;
+
+
+	void Start () {
+		menuObjects = GameObject.FindGameObjectsWithTag ("MainMenu");
+		optionsObjects = GameObject.FindGameObjectsWithTag ("Options");
+
+		audio = GetComponent<AudioSource>();
+
+		toggleOptions();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			
+			toggleOptions ();
+		}
+		
+	}
+
+	public void LoadGame() {
+		toggleMenu ();
+
+	}
+
+	public void Options() {
+		UIController.toggleOptions ();
+		UIController.toggleMenu ();
+	}
+
+	public void Back() {
+		UIController.toggleOptions ();
+		UIController.toggleMenu ();
+	}
+
+	private static void toggleOptions() {
+		foreach (GameObject o in optionsObjects) {
+			o.SetActive (optionsMenu);
+		}
+		toggleOptionsState ();
+	}
+
+	private static void toggleMenu() {
+		foreach (GameObject m in menuObjects) {
+			m.SetActive (mainMenu);
+		}
+		toggleMenuState ();
+	}
+
+	public void MuteSound() {
+		if (audio.mute)
+			audio.mute = false;
+		else
+			audio.mute = true;
+	}
+
+	public void VolumeController() {
+		volumeSlider.value = volumeAudio.volume;
+	}
+
+	public void exitApplication(){
+		Application.Quit ();
+	}
+
+	private static void toggleOptionsState() {
+		UIController.optionsMenu = !UIController.optionsMenu;
+	
+	}
+
+	private static void toggleMenuState() {
+		UIController.mainMenu = !UIController.mainMenu;
+
+	}
+}
