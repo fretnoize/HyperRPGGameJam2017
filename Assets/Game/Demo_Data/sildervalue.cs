f7cd2fdaca56650ad6c4ef3f98fbecc722ff71b7
Pandarr <silentpanda@me.com>
2017-01-22 19:43:15 -0600

    Revert "Removed demo stuff"
    This reverts commit 10a3786b4e2714beed3bbf23f662213f164f7ea0.

---
+++ Assets/Game/Demo_Data/sildervalue.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets.Game.Demo_Data
+{
+    using System;
+
+    using UnityEngine.UI;
+
+    public class sildervalue : MonoBehaviour
+    {
+        public Slider slider;
+
+        public Text text;
+
+        // Use this for initialization
+        void Start () {
+		    this.slider.onValueChanged.AddListener(updateMeh);
+            this.text.text = String.Format("{0:0.00}", this.slider.value);
+        }
+
+        private void updateMeh(float arg0)
+        {
+            this.text.text = String.Format("{0:0.00}", arg0);
+        }
+    }
+}
