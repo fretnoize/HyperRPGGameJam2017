80ab43e6fbdeb1f4a3b58a947d7168a4cd05e73b
Pandarr <silentpanda@me.com>
2017-01-22 19:43:09 -0600

    Revert "spread out disc spawns"
    This reverts commit 37a60297aa3500eef67b82f57a3b38fb52383174.

--- Assets/Game/Scripts/ObjectManager.cs
+++ Assets/Game/Scripts/ObjectManager.cs
@@ -9,43 +9,19 @@ namespace Assets.Game.Scripts
 
         [Range(0, 10)]
         public int NumberOfDiscs = 4;
-        [Range(0, 75)]
-        public float spreadOfDiscs;
 
         // Use this for initialization
         void Start()
         {
             this.DiscFolder.transform.parent = this.transform;
-            var spawns = new Vector3[NumberOfDiscs];
-
             for (var i = 0; i < this.NumberOfDiscs; i++)
             {
-                var location = new Vector3(Random.Range(-38, 38), Random.Range(-4.5f, -3), -1);
-
-                // spreads out disc spawns. If `spreadOfDiscs` is too large this might take a while
-                for (var j = 0; j < 100; j++)
-                {
-                    location = new Vector3(Random.Range(-38, 38), Random.Range(-4.5f, -3), -1);
-                    if (i == 0 || (location - spawns[i - 1]).magnitude > spreadOfDiscs)
-                    {
-                        break;
-                    }
-                    if (j == 99)
-                    {
-                        Debug.LogError("Too many disc spawn attempts");
-                    }
-                }
-
-                spawns[i] = location;
-
                 var disc = GameObject.Instantiate(
                     this.DiscPrefab,
-                    location,
+                    new Vector3(Random.Range(-38, 38), Random.Range(-4.5f, -3), -1),
                     Quaternion.identity);
                 disc.name = "Disc " + i;
                 disc.parent = this.DiscFolder.transform;
-
-                if (i != 0) {Debug.Log((spawns[i] - spawns[i - 1]).magnitude);}
             }
         }
 
